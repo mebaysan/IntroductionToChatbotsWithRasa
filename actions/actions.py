42917f4d60fe422566cb280209a502513bc21137
mebaysan <menesbaysan@gmail.com>
2022-01-17 20:31:03 +0300

    updated code

--- actions/actions.py
+++ actions/actions.py
@@ -13,8 +13,7 @@ from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 
 import requests
-# BASE_URL = "http://worldtimeapi.org/api/timezone/Europe/Istanbul"
-BASE_URL = "http://worldtimeapi.org/api/timezone"
+BASE_URL = "http://worldtimeapi.org/api/timezone/"
 
 class ActionShowTimeZone(Action):
     """
@@ -26,22 +25,23 @@ class ActionShowTimeZone(Action):
         Returns:
             Text: the text we will register in `domain.py`
         """
-        return "action_show_time_zone"
+        return "action_find_timezone"
 
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
         # we can get values from slots by `tracker` object
-        city = tracker.get_slot('city')
-
-        res = requests.get(BASE_URL+city)
-        res = res.json()
-        if res['timezone']:
-            output = f"Time zone is {res['timezone']}"
-        else:
-            output = "Please type in this structure: /Area/Region"
+        target_timezone = tracker.get_slot('target_timezone')
+        try:
+            res = requests.get(BASE_URL+target_timezone)
+            res = res.json()
         
-        dispatcher.utter_message(text=output)
-        
+            if res['utc_offset']:
+                output = f"Time zone is {res['utc_offset']}"
+            else:
+                output = "Please type in this structure: Area/Region"
+        except:
+            output = 'Ops! There are too many requests on the time zone API. Please try a few moments later...'
+        dispatcher.utter_message(text=output)       
         return []
